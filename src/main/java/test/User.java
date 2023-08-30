b4fb05a75639097308a5c491a3da4cd4b2383d2d
Mohammad AbuShanab <Mohammad.AbuShanab@icsfs.internal>
2023-08-30 15:16:32 +0300

    aaa

---
+++ src/main/java/test/User.java
@@ -0,0 +1,15 @@
+package test;
+
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String password; // Hashed and salted
+    private boolean isAdmin;
+
+
+    private VirtualMachine assignedVM;
+
+    // Constructors, getters, setters
+}
---
+++ src/main/java/test/VirtualMachine.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.util.List;
+
+
+public class VirtualMachine {
+
+    private String vmId;
+    private String ipAddress;
+    private int port;
+
+    private int users;
+
+    public String getVmId() {
+        return vmId;
+    }
+
+    public void setVmId(String vmId) {
+        this.vmId = vmId;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getUsers() {
+        return users;
+    }
+
+    public void setUsers(int users) {
+        this.users = users;
+    }
+
+
+    // Constructors, getters, setters
+}
