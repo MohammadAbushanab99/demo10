da06a64317ea0ae24b9ea77b25ea9ff892016f37
MohammadAbushanab99 <MohammadAbushanab99>
2023-08-30 07:34:46 +0300

    aa

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM openjdk:17
 
 COPY ./target/demo10-0.0.1-SNAPSHOT.jar demo10-0.0.1-SNAPSHOT.jar
 
-EXPOSE 9998
-
 CMD ["java", "-jar", "demo10-0.0.1-SNAPSHOT.jar"]
 
 
--- src/main/java/com/example/demo10/TestController.java
+++ src/main/java/com/example/demo10/TestController.java
@@ -2,7 +2,9 @@ package com.example.demo10;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/receive")
@@ -12,7 +14,6 @@ public class TestController {
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<String> receivePostRequest(@RequestBody String requestBody) {
         System.out.println("Received POST request with body: " + requestBody);
-
         String responseMessage = "Request received successfully!";
         return ResponseEntity.ok(responseMessage);
     }
